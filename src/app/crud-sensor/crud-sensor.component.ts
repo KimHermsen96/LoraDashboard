2fcaa369f6ceabc61c8694b4b4d24cb036d8e4ad
KimHermsen96 <33373651+KimHermsen96@users.noreply.github.com>
2019-11-27 15:50:24 +0100

    Revert "form validation and error"
    This reverts commit 1b413660bee4eed81ed7030b24b53419081c2938.

--- src/app/crud-sensor/crud-sensor.component.ts
+++ src/app/crud-sensor/crud-sensor.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
-import {DialogService} from '../dialog.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatButtonModule} from '@angular/material';
 
 @Component({
   selector: 'app-crud-sensor',
@@ -10,70 +9,34 @@ import {DialogService} from '../dialog.service';
 })
 export class CrudSensorComponent implements OnInit {
   checkoutForm;
-  selectedFile;
-  message: string[] = [];
+  srcResult;
 
   constructor(
-    private formBuilder: FormBuilder,
-    private http: HttpClient,
-    private dialogservice: DialogService
+    private formBuilder: FormBuilder
   ) {
     this.checkoutForm = this.formBuilder.group({
       name: '',
-      data_type: '',
-      lat: '',
-      lon: '',
+      status: ''
     });
   }
 
   ngOnInit() {
   }
 
-  onFileSelected(event) {
-    this.selectedFile = event.target.files[0];
-    console.log(this.selectedFile);
-    //hier omzetten naar dat base64
-  }
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
 
-  onSubmit(formvalue) {
-    console.log(formvalue);
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
 
-    this.validateForm(formvalue);
+      reader.onload = (e: any) => {
+        this.srcResult = e.target.result;
+      };
 
-    if (this.message.length > 0) {
-      this.dialogservice.openDialog(this.message);
-      this.message = [];
+      reader.readAsArrayBuffer(inputNode.files[0]);
     }
   }
 
-  private checkLatAndLon(formvalue, latorLon) {
 
-    console.log(formvalue);
-    let max = 180;
-    let min = -180;
-    let value = formvalue.lon;
-    let returnString = 'Longtitude is een nummer tussen de -180 en 180. Vul de numerieke waarde in.';
 
-    if (latorLon === 'lat') {
-      max = 90;
-      min = -90;
-      value = formvalue.lat;
-      returnString = 'Latitude is een nummer tussen de -90 en 90. Vul de numerieke waarde in.';
-    }
-
-    if (!value || value > max || value < min || Number.isNaN(value)) {
-      return returnString;
-    }
-  }
-
-  private validateForm(formvalue) {
-    this.addMessage(this.checkLatAndLon(formvalue, 'lat'));
-    this.addMessage(this.checkLatAndLon(formvalue, 'lon'));
-  }
-
-  private addMessage(message) {
-    if (message) {
-      this.message.push(message);
-    }
-  }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import {Observable, Subject, throwError} from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private REST_API_SERVER = 'http://192.168.1.45:3000/device/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  public sendGetRequest(query = null) {
+    if (query == null) {
+      query = 'all';
+    }
+
+    return this.httpClient.get(this.REST_API_SERVER + query).pipe(retry(3), catchError(this.handleError));
+  }
+}
+
+export class MyService {
+  myMethod$: Observable<any>;
+  private myMethodSubject = new Subject<any>();
+
+  constructor() {
+    this.myMethod$ = this.myMethodSubject.asObservable();
+  }
+
+  myMethod(data) {
+    console.log(data);
+    // we can do stuff with data if we want
+    this.myMethodSubject.next(data);
+  }
+}
